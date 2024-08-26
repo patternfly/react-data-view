bd6dc1ba53f0282d1fa288d9d1b4f27a0dd2e90d
Filip Hlavac <filip.hlavac01@gmail.com>
2024-08-26 22:04:29 +0200

    fix(pagination): Enhance useDataViewPagination with URL persisting

--- packages/module/src/Hooks/pagination.test.tsx
+++ packages/module/src/Hooks/pagination.test.tsx
@@ -49,9 +49,81 @@ describe('useDataViewPagination', () => {
     expect(result.current).toEqual({ 
       onPerPageSelect: expect.any(Function),
       onSetPage: expect.any(Function),
-      page: 3,
+      page: 1,
       perPage: 50
     })
   });
 
+  it('should read pagination state from URL', () => {
+    const mockSearchParams = new URLSearchParams('page=2&perPage=10');
+    const { result } = renderHook(() =>
+      useDataViewPagination({
+        searchParams: mockSearchParams,
+        setSearchParams: jest.fn(),
+        page: 1,
+        perPage: 5,
+      })
+    );
+
+    expect(result.current).toEqual({
+      onPerPageSelect: expect.any(Function),
+      onSetPage: expect.any(Function),
+      page: 2,
+      perPage: 10,
+    });
+  });
+
+  it('should set pagination state in URL when page changes', () => {
+    const mockSetSearchParams = jest.fn();
+    const { result } = renderHook(() =>
+      useDataViewPagination({
+        searchParams: new URLSearchParams(),
+        setSearchParams: mockSetSearchParams,
+        page: 1,
+        perPage: 5,
+      })
+    );
+
+    act(() => {
+      result.current.onSetPage(undefined, 4);
+    });
+
+    expect(mockSetSearchParams).toHaveBeenCalledTimes(2);
+  });
+
+  it('should set pagination state in URL when perPage changes', () => {
+    const mockSetSearchParams = jest.fn();
+    const { result } = renderHook(() =>
+      useDataViewPagination({
+        searchParams: new URLSearchParams('page=2'),
+        setSearchParams: mockSetSearchParams,
+        page: 1,
+        perPage: 5,
+      })
+    );
+
+    act(() => {
+      result.current.onPerPageSelect(undefined, 20);
+    });
+
+    expect(mockSetSearchParams).toHaveBeenCalledWith(
+      new URLSearchParams('page=1&perPage=20')
+    );
+  });
+
+  it('should initialize URL with default values if not present', () => {
+    const mockSetSearchParams = jest.fn();
+    renderHook(() =>
+      useDataViewPagination({
+        searchParams: new URLSearchParams(),
+        setSearchParams: mockSetSearchParams,
+        page: 1,
+        perPage: 5,
+      })
+    );
+
+    expect(mockSetSearchParams).toHaveBeenCalledWith(
+      new URLSearchParams('page=1&perPage=5')
+    );
+  });
 });
