c493b6d0bd305d6dcf1d36ef3035eb274e4f1a15
Filip Hlavac <filip.hlavac01@gmail.com>
2024-09-27 09:23:56 +0200

    feat(states): Improve active states and add error state

--- packages/module/src/DataView/DataView.tsx
+++ packages/module/src/DataView/DataView.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { Stack, StackItem } from '@patternfly/react-core';
 import { DataViewSelection, InternalContextProvider } from '../InternalContext';
 
+export const DataViewState = {
+  empty: 'empty',
+  loading: 'loading',
+  error: 'error'
+} as const;
+
+export type DataViewState = typeof DataViewState[keyof typeof DataViewState];
+
 export interface DataViewProps {
   /** Content rendered inside the data view */
   children: React.ReactNode;
   /** Custom OUIA ID */
   ouiaId?: string;
   /** Selection context configuration */
-  selection?: DataViewSelection
+  selection?: DataViewSelection;
+  /** Currently active state */
+  activeState?: DataViewState;
 }
 
 export type DataViewImpementationProps = Omit<DataViewProps, 'onSelect' | 'isItemSelected' | 'isItemSelectDisabled'>;
@@ -25,8 +35,8 @@ const DataViewImplementation: React.FC<DataViewImpementationProps> = ({
   </Stack>
 )
 
-export const DataView: React.FC<DataViewProps> = ({ children, selection, ...props }: DataViewProps) => (
-  <InternalContextProvider selection={selection}>
+export const DataView: React.FC<DataViewProps> = ({ children, selection, activeState, ...props }: DataViewProps) => (
+  <InternalContextProvider selection={selection} activeState={activeState} >
     <DataViewImplementation {...props}>{children}</DataViewImplementation>
   </InternalContextProvider>
 );
