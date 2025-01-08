e8b2f6f3f9b0e7451a9e5b72742def2cf9d9f27b
Donald Labaj <donald.labaj@gmail.com>
2025-01-08 15:27:18 -0500

    chore: release 6.1

--- packages/module/patternfly-docs/generated/index.js
+++ packages/module/patternfly-docs/generated/index.js
@@ -1,61 +1,38 @@
 module.exports = {
-  '/extensions/data-view/about-data-view/extensions': {
-    id: "About data view",
-    title: "About data view",
-    toc: [],
-    section: "extensions",
-    subsection: "Data view",
-    source: "extensions",
-    tabName: null,
-    sortValue: 1,
-    Component: () => import(/* webpackChunkName: "extensions/data-view/about-data-view/extensions/index" */ './extensions/data-view/about-data-view/extensions')
-  },
-  '/extensions/data-view/layout/react': {
-    id: "Layout",
-    title: "Layout",
-    toc: [[{"text":"Layout example"},{"text":"Predefined layout components"}]],
-    examples: ["Layout example","Predefined layout components"],
+  '/extensions/data-view/toolbar/react': {
+    id: "Toolbar",
+    title: "Data view toolbar",
+    toc: [[{"text":"Toolbar example"}],{"text":"Toolbar actions"},[{"text":"Actions example"}],{"text":"Pagination"},[{"text":"Pagination state"},{"text":"Pagination example"}],{"text":"Selection"},[{"text":"Selection state"},{"text":"Selection example"}],{"text":"Filters"},[{"text":"Filters state"},{"text":"Filtering example"}]],
+    examples: ["Toolbar example","Actions example","Pagination example","Selection example","Filtering example"],
     section: "extensions",
     subsection: "Data view",
     source: "react",
     tabName: null,
     sortValue: 2,
-    Component: () => import(/* webpackChunkName: "extensions/data-view/layout/react/index" */ './extensions/data-view/layout/react')
+    Component: () => import(/* webpackChunkName: "extensions/data-view/toolbar/react/index" */ './extensions/data-view/toolbar/react')
   },
-  '/extensions/data-view/functionality/react': {
-    id: "Functionality",
-    title: "Functionality",
-    toc: [[{"text":"Toolbar usage"},{"text":"Pagination state"},{"text":"Pagination example"},{"text":"Toolbar usage","id":"toolbar-usage-0"},{"text":"Selection state"},{"text":"Selection example"},{"text":"Toolbar usage","id":"toolbar-usage-1"},{"text":"Filters state"},{"text":"Filtering example"},{"text":"Sort state"},{"text":"Sorting example"}]],
-    examples: ["Pagination example","Selection example","Filtering example","Sorting example"],
+  '/extensions/data-view/table/react': {
+    id: "Table",
+    title: "Data view table",
+    toc: [{"text":"Configuring rows and columns"},[{"text":"Table example"}],{"text":"Tree table"},[{"text":"Tree table example"}],{"text":"Sorting"},[{"text":"Sorting example"},{"text":"Sorting state"}],{"text":"States"},[{"text":"Empty"},{"text":"Error"},{"text":"Loading"}]],
+    examples: ["Table example","Tree table example","Sorting example","Empty","Error","Loading"],
     section: "extensions",
     subsection: "Data view",
     source: "react",
     tabName: null,
     sortValue: 3,
-    Component: () => import(/* webpackChunkName: "extensions/data-view/functionality/react/index" */ './extensions/data-view/functionality/react')
+    Component: () => import(/* webpackChunkName: "extensions/data-view/table/react/index" */ './extensions/data-view/table/react')
   },
-  '/extensions/data-view/events-context/react': {
-    id: "Events context",
-    title: "Events context",
-    toc: [[{"text":"Row click subscription example"}]],
-    examples: ["Row click subscription example"],
+  '/extensions/data-view/overview/extensions': {
+    id: "Overview",
+    title: "Data view overview",
+    toc: [[{"text":"Layout"},{"text":"Modularity"}],{"text":"Events context"},[{"text":"Row click subscription example"}]],
+    examples: ["Layout","Modularity","Row click subscription example"],
     section: "extensions",
     subsection: "Data view",
-    source: "react",
+    source: "extensions",
     tabName: null,
-    sortValue: 3,
-    Component: () => import(/* webpackChunkName: "extensions/data-view/events-context/react/index" */ './extensions/data-view/events-context/react')
-  },
-  '/extensions/data-view/components/react': {
-    id: "Components",
-    title: "Components",
-    toc: [{"text":"Data view toolbar"},[{"text":"Basic toolbar example"},{"text":"Actions configuration"},{"text":"Actions example"}],{"text":"Data view table"},[{"text":"Rows and columns customization"},{"text":"Tree table example"},{"text":"Empty state example"},{"text":"Error state example"},{"text":"Loading state example"}]],
-    examples: ["Basic toolbar example","Actions example","Rows and columns customization","Tree table example","Empty state example","Error state example","Loading state example"],
-    section: "extensions",
-    subsection: "Data view",
-    source: "react",
-    tabName: null,
-    sortValue: 4,
-    Component: () => import(/* webpackChunkName: "extensions/data-view/components/react/index" */ './extensions/data-view/components/react')
+    sortValue: 1,
+    Component: () => import(/* webpackChunkName: "extensions/data-view/overview/extensions/index" */ './extensions/data-view/overview/extensions')
   }
 };
