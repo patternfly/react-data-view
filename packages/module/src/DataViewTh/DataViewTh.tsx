b020101b002fb76c508c36a66282dcf5207764ac
Katie McFaul <kmcfaul@redhat.com>
2025-09-23 16:18:17 -0400

    remove preventDefault for non arrow keys

--- packages/module/src/DataViewTh/DataViewTh.tsx
+++ packages/module/src/DataViewTh/DataViewTh.tsx
@@ -223,9 +223,6 @@ export const DataViewTh: FC<DataViewThProps> = ({
     }
 
     if (key !== 'ArrowLeft' && key !== 'ArrowRight') {
-      if (isResizing) {
-        e.preventDefault();
-      }
       return;
     }
     e.preventDefault();
